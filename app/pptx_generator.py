66663ad56d48290f74e3b38ae24f6ed0c19458cf
Joel <joelirawanlim@gmail.com>
2024-03-15 13:10:45 +0800

    Clean up code and add comments.

--- app/pptx_generator.py
+++ app/pptx_generator.py
@@ -11,7 +11,7 @@ from rich.table import Table
 # from textual.app import App, ComposeResult
 # from textual.widgets import Button, Digits
 
-
+# Puts values on all the gloabal variables
 def initialize():
     global console
     global data 
@@ -21,26 +21,24 @@ def initialize():
     global song_2_id
     global song_3_id
     console = Console()
-    # print(f"Current Working Directory: {os.getcwd()}")
+    # Gets the path of the script. As the script might be downloaded in different locations. 
     script_path = os.path.abspath(__file__)
     script_dir = os.path.split(script_path)[0]
-    # print(script_dir)
-    # print(f"Python Interpreter: {sys.executable}")
-    # pptx_file = importlib.resources.resource_filename('app', 'data/Remaja_template.pptx')
-    # with resources.files('app.data').joinpath('Remaja_template.pptx') as pptx_path:
-    #     prs = Presentation(pptx_path)
-    #     pptx_file = str(pptx_path)
+    # Comines the path where the script is locaed with the relative position of the yaml file
     with open(os.path.join(script_dir, "data/data.yml"), 'r') as file:
         data = yaml.safe_load(file)
 
+    # Combines the path of where the script is located with the relative location of where the template Presentation is. 
     pptx_path = os.path.join(script_dir, "data/Remaja_template.pptx")
     prs = Presentation(pptx_path)
+    # The output presentation's path and name. This is a placeholder so that it still generates a presentation when no name is provided. 
     output_presentation = "presentation.pptx"
-    # ------------------- Song ID -------------------------------
+    # Placeholder Song_ID
     song_1_id = '003'
     song_2_id = '001'
     song_3_id = '004'
 
+# Gets the date of the next wednesday. 
 def get_next_weekday(startdate, weekday):
     """
     @startdate: given date, in format '2013-05-25'
@@ -50,7 +48,7 @@ def get_next_weekday(startdate, weekday):
     t = timedelta((7 + weekday - d.weekday()) % 7)
     return (d + t).strftime('%d %B %Y')
 
-
+# Gets the object of the song based on the song_id. Song id k1 will give the object of k1
 def id_to_song():
     global song_1
     global song_2 
@@ -59,6 +57,7 @@ def id_to_song():
     song_2 = data[song_2_id]
     song_3 = data[song_3_id]
 
+# Converts verse_number into a string, which is useable for the script
 def generate_verse_number(verse_num): 
     if isinstance(verse_num, int):
         return str(verse_num)
@@ -66,79 +65,66 @@ def generate_verse_number(verse_num):
         return verse_num
     else: 
         return 0
+
+# Adds the first silde which says "Welcome to Remaja"
 def add_welcome_slide():
-    # print("Welcome Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[0])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     date_text = slide.placeholders[10]
     church_name = slide.placeholders[11]
     title.text = "Welcome to Remaja"
-    # date_text.text = datetime.strptime(saturday,'%d %B %Y')
     date_text.text = get_next_weekday(datetime.today().strftime('%d %B %Y'), 5)
     church_name.text = "Reformed Evangelical Church Singapore"
 
+# Adds the title slide for the first song, with the proper template and color
 def add_first_song_title_slide():
-    # print("First Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[1])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_1['title']
     number_text = slide.placeholders[10]
     author_text = slide.placeholders[11]
     author_text.text = author_find(song_1_id)
     number_text.text = "1"
 
+# Adds the title slide for the second song, with the proper template and color
 def add_second_song_title_slide():
-    # print("Second Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[2])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_2['title']
     number_text = slide.placeholders[10]
     author_text = slide.placeholders[11]
     author_text.text = author_find(song_2_id)
     number_text.text = "2"
 
+# Adds the title slide for the third song, with the proper template and color
 def add_third_song_title_slide():
-    # print("Third Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[3])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_3['title']
     number_text = slide.placeholders[10]
     author_text = slide.placeholders[11]
     author_text.text = author_find(song_3_id)
     number_text.text = "3"
     
+# Adds the lyric slides for the first song, with the proper template and color
 def add_first_song_lyrics_slide(verse_number):
-    # print("First Lyric Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[4])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_1['title']
     author_text = slide.placeholders[11]
     number_text = slide.placeholders[13]
     lyrics_text = slide.placeholders[12].text_frame
     author_text.text = author_find(song_1_id)
-    # print(verse_number)
     lyrics_text.text = song_1['lyrics'][verse_number][0]
     number_text.text = generate_verse_number(song_1['verse_number'][verse_number])
     for i in range(1, len(song_1['lyrics'][verse_number])):
         p = lyrics_text.add_paragraph()
         p.text = song_1['lyrics'][verse_number][i]
 
+# Adds the lyric slides for the second song, with the proper template and color
 def add_second_song_lyrics_slide(verse_number):
-    # print("First Lyric Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[5])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_2['title']
     author_text = slide.placeholders[11]
     number_text = slide.placeholders[13]
@@ -150,12 +136,10 @@ def add_second_song_lyrics_slide(verse_number):
         p = lyrics_text.add_paragraph()
         p.text = song_2['lyrics'][verse_number][i]
 
+# Adds the lyric slides for the third song, with the proper template and color
 def add_third_song_lyrics_slide(verse_number):
-    # print("First Lyric Slide")
     slide = prs.slides.add_slide(prs.slide_layouts[6])
     title = slide.shapes.title
-    # for shape in slide.placeholders:
-    #  print('%d %s' % (shape.placeholder_format.idx, shape.name))
     title.text = song_3['title']
     author_text = slide.placeholders[11]
     number_text = slide.placeholders[13]
@@ -167,9 +151,15 @@ def add_third_song_lyrics_slide(verse_number):
         p = lyrics_text.add_paragraph()
         p.text = song_3['lyrics'][verse_number][i]    
 
+# Adds a black blank slide. 
 def add_black_slide():
     prs.slides.add_slide(prs.slide_layouts[7])
 
+# Add multiple slides in the announcement time. 
+# Includes the slide which says announcements, 
+#  A blank white slide for placing images of the announcement,
+# The birthday song,
+# And the "See you next week!" Slide
 def add_announcements():
     prs.slides.add_slide(prs.slide_layouts[8])
     prs.slides.add_slide(prs.slide_layouts[9])
@@ -177,24 +167,33 @@ def add_announcements():
     birthday.shapes.title.text = "Birthday Song"
     prs.slides.add_slide(prs.slide_layouts[11])
 
+# Function which run the above functions for generating individual slides for the first slide. 
+# Runs the title slide, the appropiate number of lyric slides, and a blank black slide
 def generate_first_song():
     add_first_song_title_slide()
     for i in range(0, len(song_1['verse_number'])):
         add_first_song_lyrics_slide(i)
     add_black_slide()
 
+# Function which run the above functions for generating individual slides for the second slide. 
+# Runs the title slide, the appropiate number of lyric slides, and a blank black slide
 def generate_second_song():
     add_second_song_title_slide()
     for i in range(0, len(song_2['verse_number'])):
         add_second_song_lyrics_slide(i)
     add_black_slide()
 
+# Function which run the above functions for generating individual slides for the third slide. 
+# Runs the title slide, the appropiate number of lyric slides, and a blank black slide
 def generate_third_song():
     add_third_song_title_slide()
     for i in range(0, len(song_3['verse_number'])):
         add_third_song_lyrics_slide(i)
     add_black_slide()
 
+# Formats the text to be lowercase, removes any _ and -
+# Checks if the first 3 letters are "kri", and if so, removes any 0 directly behind the kri text (K0000010 becones K10)
+# If it is only a k, it removes any 0 behind kri and removes it. And replaces k with kri. 
 def text_formater(text):
     text = text.lower().replace("_", "").replace("-", "")
     if text[0] == "k":
@@ -210,6 +209,8 @@ def text_formater(text):
             return "kri" + text_temporary
     else:
         return text
+
+# Checks if the song id is a valid song id inside the song-list
 def song_id():
     global song_1_id
     global song_2_id
@@ -249,6 +250,7 @@ def author_find(key):
 
 #----------------------------- MAIN FUNCTION -----------------------------------------------------------------
 
+# This is the main function, which is run when activating pptx-generator
 def main():
     global output_presentation
     initialize()
@@ -286,6 +288,7 @@ def main():
     else:
         console.print("[red bold]Not enough arguments provided. Requires at least 3 values")
 
+# This is the search function, which is run when doing pptx-generator-search
 def search():
     global output_presentation
     initialize()
@@ -334,11 +337,7 @@ def search():
     else: 
         print('pptx-generator-search requires 2 values. \nThe first is the author, title, or KRI number of the song surrounded in double quotes ("), \nNext is the flag. -a for author, -t for title, and -k for kri number\nExample: pptx-generator-search "Grace Alone" -t')
 
-# class pptx_generator(App):
-#     def compose(self):
-#         yield Button("Start")
-
-
+# Checks if the function is run using the run button in VScode, and then activates the main function. 
 if __name__ == "__main__":
     # pptx_generator().run()
     main()
