5182d2f3475cb12f8ee3dacbf20c224f26b018ac
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-07-02 13:10:56 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template
+import data
 
 app = Flask(__name__)
 
+data_from_file = data.get_data()
 @app.route("/")
 def main():
    return render_template("index.html")
