ee3f325308d71e9ee7e93a6e0116356e6aa6d045
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-07-02 13:10:04 +0800

    Create data.py

---
+++ app/data.py
@@ -0,0 +1,13 @@
+def get_data():
+  return {
+    "example_data": "hello",
+    "example_data2": 10,
+    "example_data3": 3.14
+  }
+
+def get_song():
+  return {
+    "name": "John",
+    "email": "JohnDoe@gmail.com"
+  }
+
