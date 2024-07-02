4c53f5e7b030b4085ad1151f40886e713c5897c0
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-07-02 13:12:48 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 data_from_file = data.get_data()
 @app.route("/")
 def main():
-   return render_template("index.html")
+   return render_template("index.html", data=data_from_file)
 
 if __name__=="__main__":
    app.run(debug=True)
