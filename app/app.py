0bccd0cceec6913443980c1085465c48c1201b6e
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-08-07 08:29:30 +0000

    linked webpage with flask

--- app/app.py
+++ app/app.py
@@ -1,13 +1,27 @@
-from flask import Flask,render_template
-import data
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-data_from_file = data.get_data()
-@app.route("/")
-def main():
-   return render_template("index.html", data=data_from_file)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get data from form
+        search_query = request.form['searchquery']
+        input2 = request.form['input2']
+        input3 = request.form['input3']
+        input4 = request.form['input4']
+        input5 = request.form['input5']
 
-if __name__=="__main__":
-   app.run(debug=True)
-   
+        # Save or print for now
+        print(f"Input 1: {search_query}")
+        print(f"Input 2: {input2}")
+        print(f"Input 3: {input3}")
+        print(f"Input 4: {input4}")
+        print(f"Input 5: {input5}")
+
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
