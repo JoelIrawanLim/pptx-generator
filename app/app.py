2904910c235ba027becb1e1ea83bb7ba10e87783
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-08-09 04:51:01 +0000

    redesigned ui, added tags

--- app/app.py
+++ app/app.py
@@ -10,18 +10,23 @@ def home():
 def search_function():
    if request.method == 'POST':
       search_query = request.form.get('searchquery')
+      search_option = request.form.get('searchoption')
       # Insert code here, etc what you want to do with the variable
       print(search_query)
+      print(search_option)
       return render_template('index.html', message="Search submitted")
    else:
       return render_template('index.html')
-@app.route('/generator-function', methods=['POST'])
+@app.route('/generator-function', methods=['GET','POST'])
 def generator_function():
    if request.method == 'POST':
       song1 = request.form.get('song1')
       song2 = request.form.get('song2')
       song3 = request.form.get('song3')
       presentationtitle = request.form.get('pptxtitle')
+      if presentationtitle == "":
+         presentationtitle = 'presentation'
+      # Insert code here, etc what you want to do with the variable
       print(song1)
       print(song2)
       print(song3)
