a1a049c804f7c3cee27a9a4ba0cf2c97c7a367b6
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-10-28 22:37:12 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,6 +38,6 @@ def generator_function():
    else:
       return render_template('index.html')
       
-if __name__ == "__main__":
-   app.run(debug=True)
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
