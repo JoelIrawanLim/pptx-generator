39282f91e4f1a7939514c469e256ea5ff1c4e801
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-12-02 12:55:10 +0000

    fixed all

--- app/app.py
+++ app/app.py
@@ -10,11 +10,9 @@ def home():
 
 @app.route('/pptx-search', methods=['GET','POST'])
 def search_function():
-   global search_query,search_option
    search_query = request.form.get('searchquery')
    search_option = request.form.get('searchoption')
-   pptx_generator_ui.searchvariables(search_query,search_option)
-   pptx_generator_ui.search()
+   pptx_generator_ui.search(search_query, search_option)
 
     # Prepare the result data
    result = {
@@ -42,25 +40,23 @@ def search_function():
             invalid=pptx_generator_ui.searchresult_invalid
         )
 
-   return render_template('search.html',key = pptx_generator_ui.searchresult_key,author = pptx_generator_ui.searchresult_author,title = pptx_generator_ui.searchresult_title,error = pptx_generator_ui.searchresult_error, invalid = pptx_generator_ui.searchresult_invalid)
+   
 @app.route('/generator-function', methods=['GET','POST'])
 def generator_function():
-   global song_1,song_2,song_3
    if request.method == 'POST':
-      song_1 = request.form.get('song1')
-      song_2 = request.form.get('song2')
-      song_3 = request.form.get('song3')
+      first_song_id = request.form.get('song1')
+      second_song_id = request.form.get('song2')
+      third_song_id = request.form.get('song3')
       presentation_title = request.form.get('pptxtitle')
       if presentation_title == "":
-         presentation_title = 'presentation'
-      pptx_generator_ui.songvariables(song_1,song_2,song_3,presentation_title)
-      pptx_generator_ui.main()
-
+         presentation_title = "presentation"
+      pptx_generator_ui.main(first_song_id, second_song_id,third_song_id, presentation_title)
+      """
       @after_this_request
       def remove_file(response):
          os.remove(pptx_generator_ui.output_presentation)
          return response
-       
+      """ 
       return send_file(pptx_generator_ui.output_presentation,as_attachment=True)
    else:
       return render_template('index.html')
--- app/pptx_generator_ui.py
+++ app/pptx_generator_ui.py
@@ -4,21 +4,24 @@ import os
 import yaml
 import sys
 
+# global variables for search
+search_query = ""  #user input
+search_option = "" # -a, -t or -k
+searchresult_key = []
+searchresult_title = []
+searchresult_author = []
+searchresult_error = ""
+searchresult_invalid = ""
 
-# variable initialization
-def searchvariables(search_query, search_option):
-   global searchquery, searchoption
-   searchquery = search_query
-   searchoption = search_option
-
-
-def songvariables(song_1, song_2, song_3, presentation_title):
-   global song1, song2, song3, presentationtitle
-   song1 = song_1
-   song2 = song_2
-   song3 = song_3
-   presentationtitle = presentation_title
-
+# global variables for generate
+first_song = [] 
+second_song = []
+third_song = []
+first_song_id = ""
+second_song_id = ""
+third_song_id = ""
+presentation_title = ""
+output_presentation = ""
 
 # main app
 # Gets the date of the next wednesday.
@@ -35,6 +38,7 @@ def get_next_weekday(startdate, weekday):
 def initialize():
    global data
    global prs
+   global script_dir
    # Gets the path of the script. As the script might be downloaded in different locations.
    script_path = os.path.abspath(__file__)
    script_dir = os.path.split(script_path)[0]
@@ -46,13 +50,13 @@ def initialize():
    prs = Presentation(pptx_path)
 
 
-def id_to_song():
-   global song_1
-   global song_2
-   global song_3
-   song_1 = data[song1]
-   song_2 = data[song2]
-   song_3 = data[song3]
+def id_to_song(first_song_id, second_song_id, third_song_id):
+   global first_song
+   global second_song
+   global third_song
+   first_song = data[first_song_id]
+   second_song = data[second_song_id]
+   third_song = data[third_song_id]
 
 
 def generate_verse_number(verse_num):
@@ -81,21 +85,22 @@ def add_welcome_slide():
    add_black_slide()
 
 
-def author_find(key):
-   text = data[key]['author'][0]
-   for i in range(1, len(data[key]['author'])):
-      text = f"{text}, {data[key]['author'][i]}"
+def find_author(key):
+   text = key['author'][0]
+   for i in range(1, len(key['author'])):
+      text = f"{text}, {key['author'][i]}"
    return text
 
 
 # Adds the title slide for the first song, with the proper template and color
 def add_first_song_title_slide():
+   global first_song
    slide = prs.slides.add_slide(prs.slide_layouts[1])
    title = slide.shapes.title
-   title.text = song_1['title']
+   title.text = first_song['title']
    number_text = slide.placeholders[10]
    author_text = slide.placeholders[11]
-   author_text.text = author_find(song1)
+   author_text.text = find_author(first_song)
    number_text.text = "1"
 
 
@@ -103,10 +108,10 @@ def add_first_song_title_slide():
 def add_second_song_title_slide():
    slide = prs.slides.add_slide(prs.slide_layouts[2])
    title = slide.shapes.title
-   title.text = song_2['title']
+   title.text = second_song['title']
    number_text = slide.placeholders[10]
    author_text = slide.placeholders[11]
-   author_text.text = author_find(song2)
+   author_text.text = find_author(second_song)
    number_text.text = "2"
 
 
@@ -114,10 +119,10 @@ def add_second_song_title_slide():
 def add_third_song_title_slide():
    slide = prs.slides.add_slide(prs.slide_layouts[3])
    title = slide.shapes.title
-   title.text = song_3['title']
+   title.text = third_song['title']
    number_text = slide.placeholders[10]
    author_text = slide.placeholders[11]
-   author_text.text = author_find(song3)
+   author_text.text = find_author(third_song)
    number_text.text = "3"
 
 
@@ -126,21 +131,21 @@ def add_third_song_title_slide():
 def add_first_song_lyrics_slide(verse_number):
    slide = prs.slides.add_slide(prs.slide_layouts[4])
    title = slide.shapes.title
-   title.text = song_1['title']
+   title.text = first_song['title']
    author_text = slide.placeholders[11]
    number_text = slide.placeholders[13]
    lyrics_text = slide.placeholders[12].text_frame
-   author_text.text = author_find(song1)
-   if (verse_number + 1 > len(song_1['lyrics'])):
+   author_text.text = find_author(first_song)
+   if (verse_number + 1 > len(first_song['lyrics'])):
       # Fix this afterward
       print("An Error Occured.")
       return 0
-   lyrics_text.text = song_1['lyrics'][verse_number][0]
+   lyrics_text.text = first_song['lyrics'][verse_number][0]
    number_text.text = generate_verse_number(
-       song_1['verse_number'][verse_number])
-   for i in range(1, len(song_1['lyrics'][verse_number])):
+       first_song['verse_number'][verse_number])
+   for i in range(1, len(first_song['lyrics'][verse_number])):
       p = lyrics_text.add_paragraph()
-      p.text = song_1['lyrics'][verse_number][i]
+      p.text = first_song['lyrics'][verse_number][i]
 
 
 # Adds the lyric slides for the second song, with the proper template and color
@@ -148,21 +153,21 @@ def add_first_song_lyrics_slide(verse_number):
 def add_second_song_lyrics_slide(verse_number):
    slide = prs.slides.add_slide(prs.slide_layouts[5])
    title = slide.shapes.title
-   title.text = song_2['title']
+   title.text = second_song['title']
    author_text = slide.placeholders[11]
    number_text = slide.placeholders[13]
    lyrics_text = slide.placeholders[12].text_frame
-   author_text.text = author_find(song2)
+   author_text.text = find_author(second_song)
    number_text.text = generate_verse_number(
-       song_2['verse_number'][verse_number])
-   if (verse_number + 1 > len(song_2['lyrics'])):
+       second_song['verse_number'][verse_number])
+   if (verse_number + 1 > len(second_song['lyrics'])):
       # Fix this afterward
       print("An Error Occured.")
       return 0
-   lyrics_text.text = song_2['lyrics'][verse_number][0]
-   for i in range(1, len(song_2['lyrics'][verse_number])):
+   lyrics_text.text = second_song['lyrics'][verse_number][0]
+   for i in range(1, len(second_song['lyrics'][verse_number])):
       p = lyrics_text.add_paragraph()
-      p.text = song_2['lyrics'][verse_number][i]
+      p.text = second_song['lyrics'][verse_number][i]
 
 
 # Adds the lyric slides for the third song, with the proper template and color
@@ -170,21 +175,21 @@ def add_second_song_lyrics_slide(verse_number):
 def add_third_song_lyrics_slide(verse_number):
    slide = prs.slides.add_slide(prs.slide_layouts[6])
    title = slide.shapes.title
-   title.text = song_3['title']
+   title.text = third_song['title']
    author_text = slide.placeholders[11]
    number_text = slide.placeholders[13]
    lyrics_text = slide.placeholders[12].text_frame
-   author_text.text = author_find(song3)
+   author_text.text = find_author(third_song)
    number_text.text = generate_verse_number(
-       song_3['verse_number'][verse_number])
-   if (verse_number + 1 > len(song_3['lyrics'])):
+       third_song['verse_number'][verse_number])
+   if (verse_number + 1 > len(third_song['lyrics'])):
       # Fix this afterward
       print("An Error Occured.")
       return 0
-   lyrics_text.text = song_3['lyrics'][verse_number][0]
-   for i in range(1, len(song_3['lyrics'][verse_number])):
+   lyrics_text.text = third_song['lyrics'][verse_number][0]
+   for i in range(1, len(third_song['lyrics'][verse_number])):
       p = lyrics_text.add_paragraph()
-      p.text = song_3['lyrics'][verse_number][i]
+      p.text = third_song['lyrics'][verse_number][i]
 
 
 # Add multiple slides in the announcement time.
@@ -226,91 +231,77 @@ def add_announcements():
 
 def generate_first_song():
    add_first_song_title_slide()
-   for i in range(0, len(song_1['verse_number'])):
+   for i in range(0, len(first_song['verse_number'])):
       add_first_song_lyrics_slide(i)
    add_black_slide()
 
 
 def generate_second_song():
    add_second_song_title_slide()
-   for i in range(0, len(song_2['verse_number'])):
+   for i in range(0, len(second_song['verse_number'])):
       add_second_song_lyrics_slide(i)
    add_black_slide()
 
 
 def generate_third_song():
    add_third_song_title_slide()
-   for i in range(0, len(song_3['verse_number'])):
+   for i in range(0, len(third_song['verse_number'])):
       add_third_song_lyrics_slide(i)
    add_black_slide()
 
 
-def text_formater(text):
+def text_formatter(text):
    text = text.lower().replace("_", "").replace("-", "")
    return text
 
 
 # Checks if the song id is a valid song id inside the song-list
-def song_id():
-   global song_1
-   global song_2
-   global song_3
-   global Fail
-   song_1 = text_formater(song1)
-   song_2 = text_formater(song2)
-   song_3 = text_formater(song3)
+def song_id_checker(first_song_id, second_song_id, third_song_id):
    Fail = 0
-   if data.get(song1):
+   if data.get(first_song_id):
       Fail += 1
    else:
-      print(f"{song_1} is not a valid song id")
-   if data.get(song2):
+      print(f"{first_song_id} is not a valid song id")
+   if data.get(second_song_id):
       Fail += 1
    else:
-      print(f"{song_2} is not a valid song id")
-   if data.get(song3):
+      print(f"{second_song_id} is not a valid song id")
+   if data.get(third_song_id):
       Fail += 1
    else:
-      print(f"{song_3} is not a valid song id")
+      print(f"{third_song_id} is not a valid song id")
    if Fail != 3:
       sys.exit()
 
 
-def main():
+def main(first_song_id, second_song_id, third_song_id, presentation_title):
    global output_presentation
    initialize()
-   song_id()
-   id_to_song()
+   song_id_checker(text_formatter(first_song_id), text_formatter(second_song_id), text_formatter(third_song_id))
+   id_to_song(first_song_id, second_song_id, third_song_id)
    add_welcome_slide()
    generate_first_song()
    generate_second_song()
    generate_third_song()
    add_announcements()
-   if presentationtitle.endswith(('.pptx')):
-      output_presentation = presentationtitle
+   if presentation_title.endswith(('.pptx')):
+      output_presentation = presentation_title
    else:
-      output_presentation = presentationtitle + ".pptx"
-   prs.save(output_presentation)
+      output_presentation = presentation_title + ".pptx"
+   prs.save(script_dir + "/"  + output_presentation)
 
 
-def search():
-   global searchresult_key,searchresult_author,searchresult_title,searchresult_error,searchresult_invalid
-   global output_presentation
+def search(search_query, search_option, searchresult_key=searchresult_key, searchresult_title=searchresult_title, searchresult_author=searchresult_author, searchresult_error=searchresult_error):
    initialize()
    found = 0
-   searchresult_key = []
-   searchresult_title = []
-   searchresult_author = []
-   searchresult_error = ""
-   searchresult_invalid =""
-   if searchoption == "-a":
+   if search_option == "-a":
       addresses = []
       for key in data:
          for index_of_all_authors in range(0, len(data[key]['author'])):
             for index_of_all_words_of_authors in range(
                 0, len(data[key]['author'][index_of_all_authors].split(" "))):
-               for index_of_all_userinput_words in range(0, len(searchquery.split(" "))):
-                  if searchquery.split( " ")[index_of_all_userinput_words].lower(
+               for index_of_all_userinput_words in range(0, len(search_query.split(" "))):
+                  if search_query.split( " ")[index_of_all_userinput_words].lower(
                   ).replace(" ", "").replace("?", "").replace("!", "").replace(
                       ".", "") == data[key]['author'][index_of_all_authors].split(
                           " ")[index_of_all_words_of_authors].lower().replace(
@@ -329,12 +320,12 @@ def search():
                         found = 1
                         addresses.append(key)
       if found != 1:
-         searchresult_error = f"There is no author named {searchquery} in our song list."
+         searchresult_error = f"There is no author named {search_query} in our song list."
 
-   elif searchoption == "-t":
+   elif search_option == "-t":
       found = 0
       for key in data:
-         if searchquery.lower().replace(" ", "").replace("?", "").replace(
+         if search_query.lower().replace(" ", "").replace("?", "").replace(
              "!",
              "").replace(".", "").replace(",", "").replace(";", "").replace(
                  ":",
@@ -349,13 +340,12 @@ def search():
             searchresult_invalid =""
             found = 1
       if found != 1:
-         searchresult_error = f"There is no song named {searchquery} in our song list."
+         searchresult_error = f"There is no song named {search_query} in our song list."
          
-            # Handle Output
-   elif searchoption == "-k":
-      if searchquery.isdigit():
+   elif search_option == "-k":
+      if search_query.isdigit():
          for key in data:
-            if int(searchquery) == data[key]['kri_number']:
+            if int(search_query) == data[key]['kri_number']:
                searchresult_key = key
                searchresult_title = data[key]['title']
                searchresult_author = data[key]['author']
@@ -363,28 +353,9 @@ def search():
                searchresult_invalid =""
                found = 1
          if found != 1:
-            searchresult_error = f"There is no kri{searchquery} in our song list."
+            searchresult_error = f"There is no kri{search_query} in our song list."
       else:
          searchresult_invalid = ("Invalid input, only accepts numbers.")
-
-def debug():
-   global song_1
-   global song_2
-   global song_3
-   global output_presentation
-   initialize()
-   add_welcome_slide()
-   for number in data:
-      if number != "empty":
-         song_1 = data[number]
-         song_2 = data[number]
-         song_3 = data[number]
-         generate_first_song()
-         generate_second_song()
-         generate_third_song()
-   add_announcements()
-   prs.save(output_presentation)
-   
 
 
 if __name__ == "__main__":
