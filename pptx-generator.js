947ab9c07df117d70e737481a5b7780a4f59c6db
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2025-02-16 10:52:51 +0000

    welcome slide

--- pptx-generator.js
+++ pptx-generator.js
@@ -11,14 +11,25 @@ pptx.defineSlideMaster({
     background: {color: "FFFFFF"},
     objects: [
         {text: { text: "Title Text", options: {x: 0 , y: 1, w: 10, h: 2 , fontSize: 24, align: "center", color: "000000", autofit: true}}},
-        {text: { text: "testing",  options: {x: 1, y: 3, w: 9, h, 1, fontSize: 12, color: "000000" , autofit: true}}}
+        {text: { text: "testing",  options: {x: 1, y: 3, w: 9, h: 1, fontSize: 12, color: "000000" , autofit: true}}}
     ],
-})
+});
+
+pptx.defineSlideMaster({
+   title: "WELCOME_SLIDE_LAYOUT",
+   background: {path: "assets/welcome_slide.jpeg"},
+   objects: [
+      {text: { text: nextSaturday, options: {x: 0.5, y: 1.6, w: 10, h:1, fontSize: 18, autofit: true, color: "F5F6F7", FontFace:"Lato"}}},
+      {text: { text:"Welcome to Remaja", options: {x: 0.5, y: 1.25, w:10, h: 3, fontSize: 60, color: "FFFFFF", FontFace:"Lato", bold:true}}},
+      // {text: {text:"Reformed Evangelical Church Singapore", options:{x: 0, y: 1, w:10, h:4.3, fontSize: 18, color: "6F7D91", align: "center"}}}
+   ],
+});
 
 
 // Slide initializers
 let addDarkSlide = () => {let slide = pptx.addSlide();slide.background = { color: "000000" };};
-let addWelcomeSlide = () => {createWelcomeSlide()};
+let addWelcomeSlide = () => {pptx.addSlide({ masterName: "WELCOME_SLIDE_LAYOUT"})};
+let addSongSlide = () => {pptx.addSlide({ masterName: "SONG_SLIDE_LAYOUT"});};
 
 // Helper Functions
 function getNextSaturday() {
@@ -27,15 +38,6 @@ function getNextSaturday() {
     nextSaturday.setDate(today.getDate() + (6 - today.getDay() + 7) % 7);
     nextSaturday = nextSaturday.toLocaleDateString('en-GB',{ day: 'numeric', month: 'long', year: 'numeric' });
     return nextSaturday;
-}
-
-function createWelcomeSlide() {
-    let slide = pptx.addSlide({
-       background: { path: "/welcome_slide.jpeg"}
-    });
-    slide.addText(nextSaturday, {x: 1, y: 2.5, fontSize: 12, color: "FFFFFF"})
-    slide.addText("Welcome to Remaja"), {x: 1, y: 2.8, fontSize: 28, color: "FFFFFF"}
-    
 }
 
 
@@ -59,9 +61,9 @@ function createWelcomeSlide() {
 
 
 // Slide Layouts
-addDarkSlide();
+//addDarkSlide();
 addWelcomeSlide();
-pptx.addSlide({ masterName: "SONG_SLIDE_LAYOUT"});
+//addSongSlide();
 
 
 // save function (for debug purposes, for now saves locally)
